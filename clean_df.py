10d094fcde101cb72b71c6889339ea4a2d19aece
fbagire <faibagire@gmail.com>
2022-05-09 23:20:37 +0200

    rewriting clean_df module

--- clean_df.py
+++ clean_df.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -6,37 +5,32 @@ import seaborn as sns
 import re
 import warnings
 
-
 df = pd.read_csv('Week1_challenge_data_source.csv')
 
-print(df.shape)
-df.head()
 
-all_cols = df.columns.to_list()
+class CleanDataframe:
+    def __init__(self, df):
+        self.df = df
 
-# Check the percentage of missing values in every column
-ax = df.isna().sum().sort_values().plot(kind='barh', figsize=(9, 12))
-plt.title('Percentage of Missing Values Per Column in XDR data', fontdict={'size': 15})
+    def check_missing_values(self):
+        # Check the percentage of missing values in every column
+        ax = self.df.isna().sum().sort_values().plot(kind='barh', figsize=(9, 12))
+        plt.title('Percentage of Missing Values Per Column in XDR data', fontdict={'size': 15})
 
-for p in ax.patches:
-    percentage = '{:,.3f}%'.format((p.get_width() / df.shape[0]) * 100)
-    width, height = p.get_width(), p.get_height()
-    x = p.get_x() + width + 0.02
-    y = p.get_y() + height / 2
-    ax.annotate(percentage, (x, y))
+        for p in ax.patches:
+            percentage = '{:,.3f}%'.format((p.get_width() / self.df.shape[0]) * 100)
+            width, height = p.get_width(), p.get_height()
+            x = p.get_x() + width + 0.02
+            y = p.get_y() + height / 2
+            ax.annotate(percentage, (x, y))
 
+    def remove_missing_values(self):
+        # Drop columns which have more than 25% missing values of the total data size
 
-def removeMissingvalues(df: pd.DataFrame) -> pd.DataFrame:
-    missing_ratio = df.isna().sum() / df.shape[0]
+        missing_ratio = self.df.isna().sum() / self.df.shape[0]
 
-    # Below code gives list of columns having more than 25% nan
-    col_to_drop = missing_ratio[missing_ratio > 0.25].keys()
+        col_to_drop = missing_ratio[missing_ratio > 0.25].keys()
 
-    df.drop(col_to_drop, axis=1, inplace=True)
+        self.df.drop(col_to_drop, axis=1, inplace=True)
 
-    return df
-
-
-df = removeMissingvalues(df)
-
-df
+        return self.df
