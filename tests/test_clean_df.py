00ef9f5c98dfe76938097e198e6d7a628d25955e
fbagire <faibagire@gmail.com>
2022-05-12 16:50:52 +0200

    unit testing updated

--- tests/test_clean_df.py
+++ tests/test_clean_df.py
@@ -6,4 +6,18 @@ sys.path.append(os.path.abspath(os.path.join('../')))
 
 from clean_df import CleanDataframe
 
+df = pd.read_excel('Week1_challenge_data_source.xlsx',
+                   dtype={'Bearer Id': str, 'IMSI': str, 'MSISDN/Number': str, 'IMEI': str,
+                          'Handset Manufacturer': str, 'Handset Type': str}, engine='openpyxl')
 
+
+class TestDFCleaner(unittest.TestCase):
+    """
+    A class for unit-testing function in the clean_df.py file
+
+    Args:
+        -----
+        unittest.TestCase this allows the new class to inherit
+        from the unittest module
+
+    """
